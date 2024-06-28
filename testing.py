416ae8c02fe7e8877c6fe36a897434f9f0eaa2c1
Abhyuday Chauhan <abhyuday2003chauhan@gmail.com>
2024-06-28 15:47:18 +0530

    Added the TransformerDecoderLayer and TransformerDecoder

--- testing.py
+++ testing.py
@@ -1,3 +1,6 @@
+import copy
+from typing import Optional, Tuple
+
 import torch
 import torch.nn as nn
 from torch import Tensor
@@ -5,10 +8,90 @@ import torch.nn.functional as F
 
 from modules import MoELayer
 
-from typing import Optional
+
+def _generate_square_subsequent_mask(
+    sz: int,
+    device: Optional[torch.device] = None,
+    dtype: Optional[torch.dtype] = None,
+) -> Tensor:
+    r"""Generate a square causal mask for the sequence.
+
+    The masked positions are filled with float('-inf'). Unmasked positions are filled with float(0.0).
+    """
+    if device is None:
+        device = torch.device("cpu")
+    if dtype is None:
+        dtype = torch.float32
+    return torch.triu(
+        torch.full((sz, sz), float("-inf"), dtype=dtype, device=device),
+        diagonal=1,
+    )
 
 
-class TransformerDecoderLayerMoE(nn.TransformerDecoderLayer):
+def _get_seq_len(src: Tensor, batch_first: bool) -> Optional[int]:
+
+    if src.is_nested:
+        return None
+    else:
+        src_size = src.size()
+        if len(src_size) == 2:
+            # unbatched: S, E
+            return src_size[0]
+        else:
+            # batched: B, S, E if batch_first else S, B, E
+            seq_len_pos = 1 if batch_first else 0
+            return src_size[seq_len_pos]
+
+
+def _detect_is_causal_mask(
+    mask: Optional[Tensor],
+    is_causal: Optional[bool] = None,
+    size: Optional[int] = None,
+) -> bool:
+    """Return whether the given attention mask is causal.
+
+    Warning:
+    If ``is_causal`` is not ``None``, its value will be returned as is.  If a
+    user supplies an incorrect ``is_causal`` hint,
+
+    ``is_causal=False`` when the mask is in fact a causal attention.mask
+       may lead to reduced performance relative to what would be achievable
+       with ``is_causal=True``;
+    ``is_causal=True`` when the mask is in fact not a causal attention.mask
+       may lead to incorrect and unpredictable execution - in some scenarios,
+       a causal mask may be applied based on the hint, in other execution
+       scenarios the specified mask may be used.  The choice may not appear
+       to be deterministic, in that a number of factors like alignment,
+       hardware SKU, etc influence the decision whether to use a mask or
+       rely on the hint.
+    ``size`` if not None, check whether the mask is a causal mask of the provided size
+       Otherwise, checks for any causal mask.
+    """
+    # Prevent type refinement
+    make_causal = is_causal is True
+
+    if is_causal is None and mask is not None:
+        sz = size if size is not None else mask.size(-2)
+        causal_comparison = _generate_square_subsequent_mask(
+            sz, device=mask.device, dtype=mask.dtype
+        )
+
+        # Do not use `torch.equal` so we handle batched masks by
+        # broadcasting the comparison.
+        if mask.size() == causal_comparison.size():
+            make_causal = bool((mask == causal_comparison).all())
+        else:
+            make_causal = False
+
+    return make_causal
+
+
+def _get_clones(module, N):
+    # FIXME: copy.deepcopy() is not defined on nn.module
+    return nn.ModuleList([copy.deepcopy(module) for i in range(N)])
+
+
+class TransformerDecoderLayer(nn.TransformerDecoderLayer):
     def __init__(
         self,
         d_model: int,
@@ -37,6 +120,8 @@ class TransformerDecoderLayerMoE(nn.TransformerDecoderLayer):
         self.moe_layer = MoELayer(
             in_features=d_model, num_projections=num_projections, loss_scale=loss_scale
         )
+        del self.linear1
+        del self.linear2
 
     def _ff_block(self, x: torch.Tensor) -> torch.Tensor:
         x, loss = self.moe_layer(x)
@@ -114,11 +199,84 @@ class TransformerDecoderLayerMoE(nn.TransformerDecoderLayer):
         return x, loss
 
 
+class TransformerDecoder(nn.TransformerDecoder):
+    def __init__(
+        self,
+        decoder_layer: "TransformerDecoderLayer",
+        num_layers: int,
+        norm: Optional[nn.Module] = None,
+    ):
+        super().__init__(decoder_layer=decoder_layer, num_layers=num_layers, norm=norm)
+
+    def forward(
+        self,
+        tgt: Tensor,
+        memory: Tensor,
+        tgt_mask: Optional[Tensor] = None,
+        memory_mask: Optional[Tensor] = None,
+        tgt_key_padding_mask: Optional[Tensor] = None,
+        memory_key_padding_mask: Optional[Tensor] = None,
+        tgt_is_causal: Optional[bool] = None,
+        memory_is_causal: bool = False,
+    ) -> Tuple[Tensor, Tensor]:
+        r"""Pass the inputs (and mask) through the decoder layer in turn.
+
+        Args:
+            tgt: the sequence to the decoder (required).
+            memory: the sequence from the last layer of the encoder (required).
+            tgt_mask: the mask for the tgt sequence (optional).
+            memory_mask: the mask for the memory sequence (optional).
+            tgt_key_padding_mask: the mask for the tgt keys per batch (optional).
+            memory_key_padding_mask: the mask for the memory keys per batch (optional).
+            tgt_is_causal: If specified, applies a causal mask as ``tgt mask``.
+                Default: ``None``; try to detect a causal mask.
+                Warning:
+                ``tgt_is_causal`` provides a hint that ``tgt_mask`` is
+                the causal mask. Providing incorrect hints can result in
+                incorrect execution, including forward and backward
+                compatibility.
+            memory_is_causal: If specified, applies a causal mask as
+                ``memory mask``.
+                Default: ``False``.
+                Warning:
+                ``memory_is_causal`` provides a hint that
+                ``memory_mask`` is the causal mask. Providing incorrect
+                hints can result in incorrect execution, including
+                forward and backward compatibility.
+
+        Shape:
+            see the docs in :class:`~torch.nn.Transformer`.
+        """
+        output = tgt
+
+        seq_len = _get_seq_len(tgt, self.layers[0].self_attn.batch_first)
+        tgt_is_causal = _detect_is_causal_mask(tgt_mask, tgt_is_causal, seq_len)
+        aux_loss = 0
+
+        for mod in self.layers:
+            output, loss = mod(
+                output,
+                memory,
+                tgt_mask=tgt_mask,
+                memory_mask=memory_mask,
+                tgt_key_padding_mask=tgt_key_padding_mask,
+                memory_key_padding_mask=memory_key_padding_mask,
+                tgt_is_causal=tgt_is_causal,
+                memory_is_causal=memory_is_causal,
+            )
+            aux_loss = loss + aux_loss
+
+        if self.norm is not None:
+            output = self.norm(output)
+
+        return output, aux_loss
+
+
 if __name__ == "__main__":
     a = torch.rand(64, 22, 256)
-    layer = TransformerDecoderLayerMoE(256, 4, 4)
+    layer = TransformerDecoderLayer(256, 4, 4)
+    layer = TransformerDecoder(decoder_layer=layer, num_layers=4)
     logits, loss = layer(a, a)
-    print(logits)
+    print(logits.shape)
     print(loss)
-
     pass
